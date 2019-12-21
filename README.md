af0838cbe767318c809aec7f116e86728284cd46
soulmachine <soulmachine@gmail.com>
2019-12-20 16:58:19 -0800

    Added more APIs

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # eos-util
 
 Powerful util library based on eosjs.
+
+## Use eosjs in typescript
+
+In order to get access to the `TextEncoding` and `TextDecoding` types, you need to add `@types/text-encoding` as a dev dependency: `npm install --save-dev @types/text-encoding`
+
+If you're using Node (not a browser) then you'll also need to make sure the `dom` lib is referenced in your `tsconfig.json`:
+
+```json
+{
+  "compilerOptions": {
+    "lib": ["xxx", "dom"]
+  }
+}
+```
+
+See <https://github.com/EOSIO/eosjs/pull/568/files>.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // import BigNumber from 'bignumber.js';
-import { JsonRpc, Serialize, Numeric } from 'eosjs';
+import { Api, JsonRpc, Serialize, Numeric } from 'eosjs';
+import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig'; // development only
 import { isValidPublic } from 'eosjs-ecc';
 
 const fetch = require('node-fetch'); // node only; not needed in browsers
@@ -30,6 +31,17 @@ function getRandomRpc() {
   return new JsonRpc(url, { fetch });
 }
 
+export function getRandomApi(privateKey: string) {
+  const rpc = getRandomRpc();
+  const api = new Api({
+    rpc,
+    signatureProvider: new JsSignatureProvider([privateKey]),
+    textDecoder: new TextDecoder(),
+    textEncoder: new TextEncoder(),
+  });
+  return api;
+}
+
 /**
  * Set a customized API endpoint.
  *
@@ -47,6 +59,110 @@ export function setApiEndpoint(apiEndpoint: string): void {
   }
 }
 
+export async function sendTransaction(actions: Serialize.Action[], api: Api): Promise<any> {
+  return api.transact(
+    {
+      actions,
+    },
+    {
+      blocksBehind: 3,
+      expireSeconds: 300,
+    },
+  );
+}
+
+export function sendEOSAction(
+  from: string,
+  to: string,
+  quantity: string,
+  memo = '',
+): Serialize.Action {
+  const action: Serialize.Action = {
+    account: 'eosio.token',
+    name: 'transfer',
+    authorization: [
+      {
+        actor: from,
+        permission: 'active',
+      },
+    ],
+    data: {
+      from,
+      to,
+      quantity: `${quantity} EOS`,
+      memo,
+    },
+  };
+
+  return action;
+}
+
+export async function sendEOS(
+  from: string,
+  privateKey: string,
+  to: string,
+  quantity: string,
+  memo = '',
+): Promise<any> {
+  const action = sendEOSAction(from, to, quantity, memo);
+
+  return sendTransaction([action], getRandomApi(privateKey));
+}
+
+export function sendEOSTokenAction(
+  from: string,
+  to: string,
+  symbol: string,
+  contract: string,
+  quantity: string,
+  memo = '',
+): Serialize.Action {
+  const action: Serialize.Action = {
+    account: contract,
+    name: 'transfer',
+    authorization: [
+      {
+        actor: from,
+        permission: 'active',
+      },
+    ],
+    data: {
+      from,
+      to,
+      quantity: `${quantity} ${symbol}`,
+      memo,
+    },
+  };
+
+  return action;
+}
+
+// EOS token is similar to ETH ERC20 token.
+/**
+ * Send EOS token to another account.
+ *
+ * @param from  The sender's EOS account
+ * @param privateKey The sender's EOS private key
+ * @param to The receiver's EOS account
+ * @param symbol Token name, capitalized alpha characters only
+ * @param contract The contract name of the token
+ * @param quantity how many to send
+ * @param memo memo
+ */
+export async function sendEOSToken(
+  from: string,
+  privateKey: string,
+  to: string,
+  symbol: string,
+  contract: string,
+  quantity: string,
+  memo = '',
+): Promise<any> {
+  const action = sendEOSTokenAction(from, to, symbol, contract, quantity, memo);
+
+  return sendTransaction([action], getRandomApi(privateKey));
+}
+
 export function numericFromName(accountName: string): string {
   const sb = new Serialize.SerialBuffer({
     textEncoder: new TextEncoder(),
@@ -56,6 +172,33 @@ export function numericFromName(accountName: string): string {
   sb.pushName(accountName);
 
   return Numeric.binaryToDecimal(sb.getUint8Array(8));
+}
+
+export async function queryTransaction(txid: string, blockNum?: number): Promise<any> {
+  const rpc = getRandomRpc();
+  const response = await rpc.history_get_transaction(txid, blockNum); // eslint-disable-line no-await-in-loop
+  if (response.transaction_id || response.id) {
+    return response;
+  }
+  throw Error('Unknown response format');
+}
+
+export async function queryEOSBalance(account: string): Promise<number> {
+  const rpc = getRandomRpc();
+  const balanceInfo = await rpc.get_currency_balance('eosio.token', account, 'EOS');
+  const balance = parseFloat(balanceInfo[0].split(' ')[0]);
+  return balance;
+}
+
+export async function queryEOSTokenBalance(
+  account: string,
+  symbol: string,
+  contract: string,
+): Promise<number> {
+  const rpc = getRandomRpc();
+  const balanceInfo = await rpc.get_currency_balance(contract, account, symbol);
+  const balance = parseFloat(balanceInfo[0].split(' ')[0]);
+  return balance;
 }
 
 /**
